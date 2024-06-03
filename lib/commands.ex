bf6616acc509986db196fd6f79075f6eb44f3717
Augustin Diabira <augustindiabira@gmail.com>
2024-06-03 15:43:29 +0200

    dumb: this man does not know how to code in elixir

--- lib/commands.ex
+++ lib/commands.ex
@@ -3,6 +3,14 @@ defmodule Phobos.Commands do
   use Alchemy.Cogs
 
   Cogs.def ping do
-    Cogs.say("Pong!")
+    Cogs.say("Hello, I am Phobos! A discord bot written in Elixir.")
+
+    Cogs.say("I am currently running on Elixir #{System.version()}")
+
+    Cogs.say("I am currently running on OTP #{System.otp_release()}")
+
+    Cogs.say("I am currently running on Phobos #{Phobos.VERSION}")
+
+    Cogs.say("I am currently running on Elixir #{System.version()}")
   end
 end
